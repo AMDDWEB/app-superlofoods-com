3855344bf4914444649d20fd700039c43846888e
Craig Allen <callen@iprosystems.com>
2024-10-21 18:19:25 -0500

    Initial commit.

---
+++ android/app/src/main/java/app/rameysmarketplace/com/MainActivity.java
@@ -0,0 +1,5 @@
+package app.rameysmarketplace.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'app.rameysmarketplace.com',
+  appName: 'app-rameysmarketplace-com',
+  webDir: 'dist'
+};
+
+export default config;
